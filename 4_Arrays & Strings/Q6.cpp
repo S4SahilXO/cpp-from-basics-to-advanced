9c5bd8eb5e7b01cac0437bc389f17e3de2ecef28
Sahil Pankaj <sahilpankaj21@gmail.com>
2026-07-05 22:08:36 +0530

    Update Q6 in Arrays & Strings

--- 4_Arrays & Strings/Q6.cpp
+++ 4_Arrays & Strings/Q6.cpp
@@ -5,25 +5,31 @@ freq[ch - 'a']++ for lowercase chars
 LOGIC INSIGHT — ASCII trick: 'a' = 97, so ch - 'a' maps 'a'→0, 'b'→1 ... 'z'→25. Elegant indexing.*/
 
 #include <iostream>
+#include <vector>
 #include <string>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
-    string str;
-    cout << "Enter a string: ";
-    cin >> str;
+    
+    string s;
+    cout << "Enter any text : ";
+    getline(cin, s);
 
-    int freq[26] = {0};
+    int S_freq[26] = {0};
 
-    for (char ch : str) {
-        if (ch >= 'a' && ch <= 'z')
-            freq[ch - 'a']++;
+    for(char ch : s){
+        if(ch >='a' && ch <= 'z'){
+            S_freq[ch - 'a']++;
+        }
     }
 
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] > 0)
-            cout << char(i + 'a') << " = " << freq[i] << endl;
+    for(int i=0; i<26; i++){
+        if(S_freq[i]>0){
+            cout << char(i + 'a') << " = " << S_freq[i] << endl;
+        }
     }
-
+    
     return 0;
 }
